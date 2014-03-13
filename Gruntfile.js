8ec9e091d9f0009487dddc7f9dca4390c023e00c
William B. Wettersten <wbw20@case.edu>
2014-03-13 16:53:00 -0400

    grunt is building less assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,26 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.initConfig({
+    less: {
+      production: {
+        options: {
+          paths: ["less"],
+          cleancss: true,
+        },
+        files: {
+          "built/index.css": "less/index.less"
+        }
+      }
+    },
     watch: {
       js: {
         files: ['js/**/*.js', 'html/*.html'],
         tasks: ['concat', 'includes']
       },
     },
-    connect: {
-      server: {
-        options: {
-          port: 8080,
-          base: 'built'
-        }
-      }
-    }
   });
 
-  grunt.registerTask('default', ['concat', 'copy', 'includes']);
+  grunt.registerTask('default', ['less']);
   grunt.registerTask('server', ['connect', 'copy', 'watch']);
 };
--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/ejs');
 app.set('view engine', 'html');
 
+app.use(express.static(__dirname + '/built'));
+
 app.get('/', function(request, response) {
   response.render('index.ejs');
 });
