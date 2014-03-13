add48d41c9717bd68792931881763d0f2be73ed3
William B. Wettersten <wbw20@case.edu>
2014-03-13 16:01:18 -0400

    ejs views set properly

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express'),
     ejs = require('ejs');
 
 var app = express();
+app.engine('.html', require('ejs').__express);
+// app.set('ejs', __dirname + '/ejs');
+app.set('views', __dirname + '/ejs');
+app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.send('ok');
+  response.render('index.ejs');
 });
 
 app.listen(80);
