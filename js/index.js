1dd76d196fd38c4f88a7206ebd69dcc43dadb8d4
William B. Wettersten <wbw20@case.edu>
2014-03-15 19:02:56 -0400

    WIP loading animation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  setTimeout(function() {
-    $('.buy').css('opacity', 1.0);
-  }, 3000);
+  $('video').bind('progress', function() {
+    if ($('video').get(0).buffered.length > 0) {
+      console.log($('video').get(0).buffered.end(0) / $('video').get(0).duration);
+    }
+  });
+
+    setTimeout(function() {
+      $('.buy').css('opacity', 1.0);
+    }, 3000);
 });
