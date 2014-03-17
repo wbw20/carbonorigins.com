88bbb4bb5650787eaa94a6ae0237b57f65cce6e6
William B. Wettersten <wbw20@case.edu>
2014-03-17 15:57:40 -0400

    add check for html5 video support

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
   $('video').bind('progress', function() {
-    setTimeout(function() {
-      play();
-    }, 3000); //Max loading time of 3 seconds
+    if($('video')[0].canPlayType && $('video')[0].canPlayType( 'video/mp4; codecs="mp4v.20.8"' )) {
+      if ($('video').get(0).buffered.length > 0) {
+        var percent = 100 * ($('video').get(0).buffered.end(0) / $('video').get(0).duration);
 
-    if ($('video').get(0).buffered.length > 0) {
-      var percent = 100 * ($('video').get(0).buffered.end(0) / $('video').get(0).duration);
+        percent /= 15/100; //only wait until video is 15% loaded
 
-      percent /= 15/100; //only wait until video is 15% loaded
-
-      if (percent > 100) {
-        play();
-      } else {
-        console.log(percent);
-        $('.loading .logo').css('width', percent + '%');
+        if (percent > 100) {
+          show();
+          $('video')[0].play();
+        } else {
+          console.log(percent);
+          $('.loading .logo').css('width', percent + '%');
+        }
       }
+    } else { // html5 video not supported
+      show();
     }
+
+    setTimeout(function() {
+      show();
+      $('video')[0].play();
+    }, 3000); //Max loading time of 3 seconds
   });
 
     setTimeout(function() {
@@ -23,8 +29,7 @@ $(document).ready(function() {
     }, 7000);
 });
 
-function play() {
-  $('video')[0].play();
+function show() {
   $('.content').css('display', 'block');
   $('.loading').css('display', 'none');
   $('video').unbind('progress');
