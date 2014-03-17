41e1442469405466247e166a81e9c3dde3bf4e5c
William B. Wettersten <wbw20@case.edu>
2014-03-17 15:02:26 -0400

    set maximum loading screen duration

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   $('video').bind('progress', function() {
+    setTimeout(function() {
+      play();
+    }, 3000); //Max loading time of 3 seconds
+
     if ($('video').get(0).buffered.length > 0) {
       var percent = 100 * ($('video').get(0).buffered.end(0) / $('video').get(0).duration);
 
-      percent /= 15/100; // only wait until video is 15% loaded
+      percent /= 15/100; //only wait until video is 15% loaded
 
       if (percent > 100) {
-        $('video')[0].play();
-        $('.content').css('display', 'block');
-        $('.loading').css('display', 'none');
-        $('video').unbind('progress');
+        play();
       } else {
+        console.log(percent);
         $('.loading .logo').css('width', percent + '%');
       }
     }
@@ -20,3 +22,10 @@ $(document).ready(function() {
       $('.buy').css('opacity', 1.0);
     }, 7000);
 });
+
+function play() {
+  $('video')[0].play();
+  $('.content').css('display', 'block');
+  $('.loading').css('display', 'none');
+  $('video').unbind('progress');
+}
