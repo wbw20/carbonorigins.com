2bf8381ff60dc3534e06751ed08737b03bd19f92
William B. Wettersten <wbw20@case.edu>
2014-04-18 12:26:12 -0400

    improve caching

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
+  $('video')[0].pause();
   if ($('.loading').css('display') != 'none') {
     $('.loading .logo').width(100);
   }
 
   setTimeout(function() {
+    $('video')[0].play();
     $('.loading').css('opacity', 0.0);
-  }, 2400);
+  }, 4000);
 
   setTimeout(function() {
     $('.buy').css('opacity', 1.0);
-  }, 3000);
+  }, 4500);
 });
