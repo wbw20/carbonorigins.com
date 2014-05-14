6c5737e987c92c70c57b07c087eb5bfb88795b30
William B. Wettersten <wbw20@case.edu>
2014-05-14 16:18:02 -0400

    its flipping

--- js/apollo.js
+++ js/apollo.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   /* logic for preorder flip */
   $('#preorder').click(function(event) {
-    $('.description').animo("rotate", { degrees:90 }, function(e) {
-    });
+    $('.description').animo( { animation: "flipOutY", keep: true } );
   })
 });
