09abec9d9ddf91a55baf6ec83cd0fddb6214f240
William B. Wettersten <wbw20@case.edu>
2014-05-14 20:53:09 -0400

    faster spin

--- js/apollo.js
+++ js/apollo.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
   /* logic for preorder flip */
   $('#preorder').click(function(event) {
-    $('.description').animo( { animation: "flipOutY", keep: true } );
+    $('.description').animo({
+      animation: "flipOutY",
+      duration: 0.2,
+      keep: true
+    });
   })
 });
