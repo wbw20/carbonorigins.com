6b0981fd954a18c814223e6498790870004bb03b
William B. Wettersten <wbw20@case.edu>
2014-05-16 11:58:56 -0400

    website becomes profane

--- js/apollo.js
+++ js/apollo.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
       animation: "flipOutY",
       duration: 0.2,
       keep: true
+    }, function(event) {
+      $('.info').css('display', 'none');
+      $('.preorder').css('display', 'block');
+      $('.description').animo({
+        animation: "flipInY",
+        duration: 0.2,
+        keep: true
+      });
     });
   })
 });
